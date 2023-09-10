703c17bfe9e60971a01b357a2c9b103c70f6a377
Tom Vorel <tomvorel13@gmail.com>
2023-09-10 19:04:52 +0200

    Removing web scraping

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,9 @@
 import express from "express";
-import { returnHeadingText } from "./lib/elements";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
-});
-
-app.get("/info", async (req, res) => {
-  const heading = await returnHeadingText();
-
-  if (heading) {
-    res.status(200).send({ message: heading });
-  } else {
-    res.status(500).send({ message: "Something went wrong" });
-  }
 });
 
 app.listen(3000, () => {
